3a843472d2a5ca3e5c1f363c4f122f4579baee50
Joe Koberg <joe.koberg@versionone.com>
2013-12-14 19:27:47 -0500

    Simplify example by returning next step directly without F# async conversion

--- Fowin.Apps/DummyApp.fs
+++ Fowin.Apps/DummyApp.fs
@@ -5,19 +5,13 @@ open System.Collections.Generic
 open System.Threading.Tasks
 open Fowin.Types
 
-let writeHello(body:IO.Stream) =
-  let b = "<p>Hello, World!</p>"B
-  body.Write(b, 0, b.Length)
-
 let DummyApp (next:AppFunc) =
   Func<_,_>(fun (env:OwinEnv) ->
-    start <| async {
-
       match env.["owin.ResponseBody"] with
-      | :? IO.Stream as s -> writeHello s
+      | :? IO.Stream as s ->
+        let b = "<p>Hello, World!</p>"B
+        s.WriteAsync(b, 0, b.Length)
       | _ -> failwith "didn't find owin.ResponseBody in environment!"
-
-    }
-  )
+    )
 
 
--- Fowin.Apps/FooMiddleware.fs
+++ Fowin.Apps/FooMiddleware.fs
@@ -7,9 +7,14 @@ open Fowin.Types
 let FooMiddleware (next:AppFunc) =
   Func<_,_>(fun (env:OwinEnv) ->
     env.["FooToken"] <- "foo!"
+    next.Invoke(env)
+  )
+
+let FooMiddleware2 (next:AppFunc) =
+  Func<_,_>(fun (env:OwinEnv) ->
+    env.["FooToken"] <- "foo!"
     start <| async {
       let! flag = next.Invoke(env) |> Async.AwaitIAsyncResult
       ignore flag
     }
   )
-
