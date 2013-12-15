a446646ff272b38d12ca5eb31908fdddff7b6e25
Joe Koberg <joe.koberg@versionone.com>
2013-12-15 16:07:17 -0500

    Change name of dictionary element

--- Fowin.Apps/BodyBuilder.fs
+++ Fowin.Apps/BodyBuilder.fs
@@ -7,20 +7,20 @@ open Fowin.Abbrevs
 type Bodylist = System.Collections.Generic.List<Task<string>>
 
 let writeBody (env:OwinEnv) s =
-  let elems : Bodylist = downcast env.["bodyparts"]
+  let elems : Bodylist = downcast env.["body.parts"]
   elems.Add(Task.FromResult(s))
 
 let finalize (env:OwinEnv) = async {
   let bodystream : IO.Stream = downcast env.["owin.ResponseBody"]
-  let elems : Bodylist = downcast env.["bodyparts"]
+  let elems : Bodylist = downcast env.["body.parts"]
   for task in elems do
     use s = new IO.StreamWriter(bodystream)
     let! elemtxt = await task
     do! await0 <| s.WriteAsync elemtxt
-    }
+  }
 
 let BodyBuilderMiddleware (next:AppFunc) (env:OwinEnv) =
-    env.["bodyparts"] <- Bodylist()
+    env.["body.parts"] <- Bodylist()
     start <| async {
       do! env |> invoke next
       do! finalize env
