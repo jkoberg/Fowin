eddc82fa730419dcec306ca2c6c860f4d7abda80
Joe Koberg <joe.koberg@versionone.com>
2013-12-14 19:15:29 -0500

    Pull out message body

--- Fowin.Apps/DummyApp.fs
+++ Fowin.Apps/DummyApp.fs
@@ -5,14 +5,16 @@ open System.Collections.Generic
 open System.Threading.Tasks
 open Fowin.Types
 
+let writeHello(body:IO.Stream) =
+  let b = "<p>Hello, World!</p>"B
+  body.Write(b, 0, b.Length)
+
 let DummyApp (next:AppFunc) =
   Func<_,_>(fun (env:OwinEnv) ->
     start <| async {
 
       match env.["owin.ResponseBody"] with
-      | :? IO.Stream as s ->
-        let b = "<p>yo</p>"B
-        s.Write(b, 0, b.Length)
+      | :? IO.Stream as s -> writeHello s
       | _ -> failwith "didn't find owin.ResponseBody in environment!"
 
     }
