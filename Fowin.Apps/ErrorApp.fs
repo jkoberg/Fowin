04efe2ba51e858f8e0b39f903839e6ef8936041c
Joe Koberg <joe.koberg@versionone.com>
2013-12-16 09:48:47 -0500

    Debugging app

---
+++ Fowin.Apps/ErrorApp.fs
@@ -0,0 +1,9 @@
+﻿module Fowin.Apps.ErrorApp
+
+open Fowin.Abbrevs
+open Fowin.Apps.BodyBuilder
+
+let ErrorApp (next:AppFunc) (env:OwinEnv) =
+    for key in env.Keys do
+      writeBody env (sprintf "%s: %A\n" key env.[key])
+    NullTask
