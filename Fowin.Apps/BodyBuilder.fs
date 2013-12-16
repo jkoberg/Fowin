94c894b55721a0e6d9c06d762fa5cb1e809756b1
Joe Koberg <joe.koberg@versionone.com>
2013-12-16 10:19:53 -0500

    un-factor the behavior of the body writer middlware

--- Fowin.Apps/BodyBuilder.fs
+++ Fowin.Apps/BodyBuilder.fs
@@ -13,16 +13,6 @@ let writeBody (env:OwinEnv) s =
   let elems : Bodylist = downcast env.["body.parts"]
   elems.Add(Task.FromResult(s))
 
-/// finalize is called by the middleware to write out the environment
-let finalize (env:OwinEnv) = async {
-  let bodystream : IO.Stream = downcast env.["owin.ResponseBody"]
-  let elems : Bodylist = downcast env.["body.parts"]
-  for task in elems do
-    use s = new IO.StreamWriter(bodystream)
-    let! elemtxt = await task
-    do! await0 <| s.WriteAsync elemtxt
-  }
-
 /// This middleware will add a List<Task<string>> to the environment,
 /// then run the downstream app `next`, and then will await
 /// and write each string to the body of the response
@@ -30,8 +20,11 @@ let finalize (env:OwinEnv) = async {
 /// `writeBody env "foo"`, or add Task<string> objects to
 /// `env.["body.parts"]` directly.
 let BodyBuilderMiddleware (next:AppFunc) (env:OwinEnv) =
+  start <| async {
     env.["body.parts"] <- Bodylist()
-    start <| async {
-      do! env |> invoke next
-      do! finalize env
-    }
+    do! env |> invoke next // call downstream middleware
+    use body = new IO.StreamWriter(stream = downcast env.["owin.ResponseBody"]) 
+    for task in downcast env.["body.parts"] do
+      let! (elemtxt:string) = await task
+      do! await0 <| body.WriteAsync elemtxt
+  }
